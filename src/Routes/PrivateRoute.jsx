75a09e75ac33a7ab44f2bc05d481b8daa445bd0c
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-13 23:24:14 +0600

    added

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProvider';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
 
     const {user, loading} = useContext(AuthContext);
+    const location = useLocation();
+    console.log(location.pathname);
 
     if(loading){
         return <span className="loading loading-infinity loading-lg"></span>
